24c796656983693c3090e8f6c187fb55f82155eb
Nattapass <kirazboonpal@gmail.com>
2023-12-02 17:41:20 +0700

    [UPDATE] file build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7NCMGSS6.mjs";import"./chunk-BRSWJE5B.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-XSAXQ4HA.mjs";import"./chunk-NKCFF4DQ.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
