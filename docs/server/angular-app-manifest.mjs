e52230003c731dbb77a1900ac98524af33ec2b4e
Nattapass <kirazboonpal@gmail.com>
2024-12-24 16:20:29 +0700

    fix css

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -6,8 +6,8 @@ export default {
   locale: undefined,
   routes: undefined,
   assets: {
-    'index.csr.html': {size: 2542, hash: 'ecaa36846da4c94027815f4142009bdf861436a06433feb3733b94fd6c7680bf', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1358, hash: 'bf347e16da666148317ef29ef9ba5e801f52080f3158d35bb46633bcf6667066', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-WKGWGCJM.css': {size: 33268, hash: '2FO2XjqqvB8', text: () => import('./assets-chunks/styles-WKGWGCJM_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 2542, hash: 'ecfe4d79a6f1eaf45badbdc1e3561201d0d1f10d6c745d250fa9827bbb2c430b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1358, hash: '66d6ea6775e25403fa5aa1d01c064e18b7efb3602942034ac046913c70ae7115', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-ES666NSD.css': {size: 41872, hash: 'polUG9fdQtw', text: () => import('./assets-chunks/styles-ES666NSD_css.mjs').then(m => m.default)}
   },
 };
